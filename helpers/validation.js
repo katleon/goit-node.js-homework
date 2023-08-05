d28cbf9e3aa62bb5910db802b1e0ebd719b53e09
Katarzyna Leoniuk <kasia.leoniuk@gmail.com>
2023-08-05 15:40:20 +0200

    Finished: homework 2
    Added: validation from Joi; routes for HTTPS methods

---
+++ helpers/validation.js
@@ -0,0 +1,18 @@
+const Joi = require("joi");
+
+const addContactSchema = Joi.object({
+  name: Joi.string().alphanum().min(3).max(50).required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().min(6).max(22).required(),
+});
+
+const changeContactSchema = Joi.object({
+  name: Joi.string().alphanum().min(3).max(50),
+  email: Joi.string().email(),
+  phone: Joi.string().min(6).max(22),
+}).or("name", "email", "phone");
+
+module.exports = {
+  addValidate: addContactSchema,
+  putValidate: changeContactSchema,
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,62 @@
-const express = require('express')
+const express = require("express");
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require("../../controller/contacts");
 
-const router = express.Router()
+const { addValidate, putValidate } = require("../../helpers/validation");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  const contactsList = await listContacts();
+  res.json(contactsList);
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const foundContact = await getContactById(req.params.contactId);
+  if (!foundContact) {
+    res.status(404).json({ message: "Not found!" });
+  } else {
+    res.json(foundContact);
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  if (addValidate.validate(req.body).error) {
+    res.status(400).json({
+      message: addValidate.validate(req.body).error.details[0].message,
+    });
+  } else {
+    const message = await addContact(req.body);
+    res.status(201).json(message);
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  const foundContact = await getContactById(req.params.contactId);
+  if (!foundContact) {
+    res.status(404).json({ message: "Not found!" });
+  } else {
+    await removeContact(req.params.contactId);
+    res.json({ message: "Contact deleted" });
+  }
+});
 
-module.exports = router
+router.put("/contactId", async (req, res, next) => {
+  const foundContact = await getContactById(req.params.contactId);
+  const error = putValidate.validate(req.body).error;
+  if (!error) {
+    const editedContact = await updateContact(req.params.contactId, req.body);
+    res.json(editedContact);
+  } else if (!foundContact) {
+    res.status(404).json({ message: "Not found!" });
+  } else if (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+module.exports = router;
