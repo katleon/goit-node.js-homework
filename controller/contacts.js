0c7e92a6a646e9aff18a7c5a89676eb73251c956
Katarzyna Leoniuk <kasia.leoniuk@gmail.com>
2023-08-10 13:50:44 +0200

    Fix

--- controller/contacts.js
+++ controller/contacts.js
@@ -1,4 +1,4 @@
-const { Contact } = require("../models/contacts.js");
+const Contact = require("../models/contacts.js");
 
 async function listContacts() {
   const contacts = await Contact.find();
--- server.js
+++ server.js
@@ -19,5 +19,5 @@ mongoose
   });
 
 app.listen(3000, () => {
-  console.log(`Server running. Use our API on port: 3000`);
+  console.log("Server running. Use our API on port: 3000");
 });
