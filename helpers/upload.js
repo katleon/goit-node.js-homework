36ac17f0185c59052ca58709b62db3aa65c6d843
Katarzyna Leoniuk <kasia.leoniuk@gmail.com>
2023-08-28 18:04:39 +0200

    fix

--- helpers/upload.js
+++ helpers/upload.js
@@ -1,10 +1,10 @@
 import multer from "multer";
 import path from "path";
 
-const tempPath = path.resolve("temp");
+const tmpPath = path.resolve("tmp");
 
 const multerConfig = multer.diskStorage({
-  destination: tempPath,
+  destination: tmpPath,
   filename: (req, file, cb) => {
     cb(null, file.originalname);
   },
